5adfbcdb0ff1945fc5e615080ff18720c865e2a6
Benimadhav63 <benimadhavchawda@gmail.com>
2023-11-29 09:15:43 +0530

    committing conditions and loops assignment

---
+++ Javascript/Conditions and Loops/Q4.js
@@ -0,0 +1,10 @@
+// Q4. Generate numbers between any 2 given n9mbers.
+
+const num1 = 10
+const num2 = 25;
+let i=num1+1;
+
+while(i<=num2){
+    console.log(i)
+    i++
+}
---
+++ Javascript/Conditions and Loops/Q5.js
@@ -0,0 +1,14 @@
+// Q5. Use the while loop to print n9mbers from 1 to 25 in ascending and descending order.
+
+let num = 1
+console.log("Ascending order")
+while(num<=25){
+    console.log(num)
+    num++
+}
+console.log("Descending order")
+num--
+while(num>=1){
+    console.log(num)
+    num--
+}
